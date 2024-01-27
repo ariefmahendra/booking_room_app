6f02b1c42d881af34ac95951fc6fccd0d9d85517
Arief Mahendra <ariefmahendra985@gmail.com>
2024-01-27 13:19:33 +0700

    Created validation access resources by role, id and email

--- usecase/auth_usecase.go
+++ usecase/auth_usecase.go
@@ -55,9 +55,7 @@ func (a *AuthUCImpl) Login(request dto.AuthRequest) (dto.AuthResponse, error) {
 	if err != nil {
 		return dto.AuthResponse{}, err
 	}
-	fmt.Println(userByEmail)
-	fmt.Println("userByEmail.password : ", userByEmail.Password)
-	fmt.Println("request.Password : ", request.Password)
+
 	if err := bcrypt.CompareHashAndPassword([]byte(userByEmail.Password), []byte(request.Password)); err != nil {
 		fmt.Println("middleware : invalid password")
 		return dto.AuthResponse{}, err
@@ -68,7 +66,8 @@ func (a *AuthUCImpl) Login(request dto.AuthRequest) (dto.AuthResponse, error) {
 		return dto.AuthResponse{}, errors.New("invalid email or email")
 	}
 
-	authResponse, err := a.jwtService.GenerateToken(userByEmail.Id, userByEmail.Role)
+	fmt.Println("Auth usecase : ", userByEmail.Id)
+	authResponse, err := a.jwtService.GenerateToken(userByEmail.Id, userByEmail.Email, userByEmail.Role)
 	if err != nil {
 		return dto.AuthResponse{}, err
 	}
